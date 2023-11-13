1173ec9373bc1a153af1c5452df6e41581706835
afpera <afpera@gmail.com>
2023-11-12 22:27:10 -0500

    Update art_YanHer.php

--- es/art_YanHer.php
+++ es/art_YanHer.php
@@ -11,28 +11,28 @@
         <div class="carousel slide carousel-dark text-center d-flex justify-content-center" data-bs-ride="carousel" data-bs-interval="7000" id="obra">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_VenEva_01.jpg">
-                        <p class="piedeobra"><span class="obraTit">Niñas, no madres </span><br>Pintura y bordado sobre tela<br>2023</p>
+                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_YanHer_01.jpg">
+                        <p class="piedeobra"><span class="obraTit">Deseos y nostalgia más allá de la maternidad. </span><br>Acrílico sobre latón&nbsp;<br>2022</p>
                     </figure>
                 </div>
                 <div class="carousel-item">
-                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_VenEva_02.jpg">
-                        <p class="piedeobra"><span class="obraTit">Añachallaw / Mi autorretrato erótico </span><br>óleo sobre lienzo<br>2023</p>
+                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_YanHer_02.jpg">
+                        <p class="piedeobra"><span class="obraTit">Parto, Serie Wawawasi.</span><br>Tinta acrílica sobre oratorio de madera.<br>2022</p>
                     </figure>
                 </div>
                 <div class="carousel-item">
-                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_VenEva_03.jpg">
-                        <p class="piedeobra"><span class="obraTit">Munanakuy (enamoramiento) </span><br>Lápices de colores sobre cartulina<br>2021</p>
+                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_YanHer_03.jpg">
+                        <p class="piedeobra"><span class="obraTit">Dentro de mi habitan montañas</span><br>Acrílico sobre cartón<br>2023</p>
                     </figure>
                 </div>
                 <div class="carousel-item">
-                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_VenEva_04.jpg">
-                        <p class="piedeobra"><span class="obraTit">Sirvinakuy (convivencia) </span><br>Lápices de colores sobre cartulina<br>2021</p>
+                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_YanHer_04.jpg">
+                        <p class="piedeobra"><span class="obraTit">Si la tierra es de todos, la tierra es de nadie</span><br>Acrílico sobre cartón<br>2021</p>
                     </figure>
                 </div>
                 <div class="carousel-item">
-                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_VenEva_05.jpg">
-                        <p class="piedeobra"><span class="obraTit">Sarhuinas emprendedoras en la ciudad de Lima </span><br>Serie <em>“El emprendimiento de la mujer sarhuina en la ciudad de Lima a través del arte y la cultura”.</em><br>Tierra de colores, acrílico sobre madera y marco textil.<br>2019</p>
+                    <figure class="figure"><img class="img-fluid figure-img" src="../assets/img/art_gal/art_gal_YanHer_05.jpg">
+                        <p class="piedeobra"><span class="obraTit">Amaru crionça</span><br>Acrílico sobre papel kraft<br>2021</p>
                     </figure>
                 </div>
             </div>
@@ -42,14 +42,14 @@
     </section>
     <div class="accordion accordion-flush" role="tablist" id="seccs">
         <div class="accordion-item cremita noMargin">
-            <h2 class="accordion-header" role="tab"><button class="accordion-button butCremita SECCS" type="button" data-bs-toggle="collapse" data-bs-target="#seccs .seccs-1" aria-expanded="true" aria-controls="seccs .seccs-1">Venuca Evanán</button></h2>
+            <h2 class="accordion-header" role="tab"><button class="accordion-button butCremita SECCS" type="button" data-bs-toggle="collapse" data-bs-target="#seccs .seccs-1" aria-expanded="true" aria-controls="seccs .seccs-1">Yanaki Herrera</button></h2>
             <div class="accordion-collapse collapse show seccs-1" role="tabpanel">
                 <div class="accordion-body">
                     <div class="row">
                         <div class="col-md-10 col-lg-6 offset-md-1 offset-xxl-2 desap">
-                            <p>Artista visual e ilustradora. De raíces ayacuchanas, Evanán destaca por su particular aproximación a las tradiciones y expresiones artísticas de la comunidad Sarhua. Su obra aborda aspectos relativos a la reivindicación del rol de la mujer y la migración. En este sentido, es una&nbsp;difusora de la ilustración de tablas de Sarhua, habiendo conducido talleres de este arte en el Museo Nacional de Arqueología, Antropología e Historia del Perú y el Lugar de la Memoria (LUM). En el año 2020 fue galardonada con el Premio de Arte Contemporáneo ICPNA.</p>
+                            <p>Yanaki Herrera es graduada en Artes Visuales en la Universidad Federal de Minas Gerais UFMG. Nacida en Cusco, Perú y actualmente viviendo en Belo Horizonte, Brasil. Su investigación artística está volcada al maternar y a las luchas colectivas y subjetivas de las madres. A través de la pintura, crea narrativas que conversan entre la ancestralidad y el presente como un lugar de transformación. Su leguaje visual es compuesto por elementos que nacen a partir de las expresiones corporales y culturales de América Latina.<br><br>Recibió la premiación <em>“Memorial Minas Vale 2021”.</em> Exposiciones individuales: <em>“Warmiwasi: Luchas colectivas y subjetivas”</em> Galería BDMG MG (2023); <em>“Tal como semillas”</em> Galería A Gentil Carioca SP (2023). Exposiciones colectivas: <em>“El cuerpo invisible de la memoria” </em>Museo da Inconfidencia MG (2023); <em>“MAA”</em> Mitre Galería MG (2023). Residencias artísticas: <em>“Residencia Cultural”</em> Oficina Francisco Brennand PE (2023); <em>“LAB.suay”</em> Piso SLZ MA (2023); <em>“Residencia Artística”</em> IA – Instituto de Arte Contemporáneo de Ouro Preto MG (2022); <em>“LAB Cultural” </em>BDMG MG (2021).&nbsp;</p>
                         </div>
-                    </div><img class="float-end art_ret" src="../assets/img/art_ret/art_ret_VenEva.png" loading="eager">
+                    </div><img class="float-end art_ret" src="../assets/img/art_ret/art_ret_YanHer.png" loading="eager">
                 </div>
             </div>
         </div>
@@ -60,19 +60,21 @@
                     <div class="row">
                         <div class="col-md-9 col-xxl-8 offset-md-1 offset-xl-1 offset-xxl-2">
                             <div class="destaque">
-                                <p> Mi intención no es hacer quedar mal a los hombres andinos, en absoluto, pero no podemos negar de que existe violencia hacia las mujeres, sea andina, amazónica o de la ciudad. Existe mucha violencia hacia las mujeres. </p>
+                                <p>Pienso que la cultura popular desde siempre se ha caracterizado por ser impactante, genuina, sofisticada, capaz de llevarnos a muchos lugares. El problema es que ese mundo del arte blanco nunca lo consideró como “arte”. Sin embargo eso está cambiando gracias a mucha lucha y resistencia de las personas que hacen parte de la cultura popular. Pienso que la cultura popular carga múltiples legados, mucha historia y es necesario re direccionar la mirada para ello. </p>
                             </div>
                             <div class="entrev">
-                                <h1 class="preg">Tu trabajo es contestatario pues visibiliza la violencia de género y la reivindicación de la mujer. ¿Esto cómo es recibido en la comunidad ayacuchana y fuera de ella?</h1>
-                                <p class="resp">Vivo en Lima, en Chorrillos y he visto en mi entorno cercano violencia de género. A mí me han violentado, por eso me divorcié. De repente no fue violencia física, pero quizá la forma de tratarme, la indiferencia, haciendo caso al individualismo, cosas que no deberían ser. Entonces yo decidí separarme. He visto en mi entorno maltrato físico, violencia económica, psicológica, sometimiento. Es más, cuando me casé una anciana de la comunidad se acercó y me dijo: “si tu marido te pega, déjate golpear”, ese fue su consejo. Y ahora entiendo la razón. Aquí nuestro estado es muy violento con las comunidades indígenas, más con las mujeres y los niños. En la comunidad no desean comisarías pues abunda la corrupción y a veces las mujeres para sobrevivir tienen que aguantar. Lamentablemente se normaliza la violencia. Lamentablemente al estado no le interesa la mujer, menos las mujeres del campo, las mujeres de las comunidades originarias. Entonces ese es un método de las mujeres para sobrevivir. Ahora entiendo mejor las cosas.<br><br>Si lo pinto, trabajo sobre este tema, porque considero que es algo que puede traer algún cambio a través del arte. Algunos me critican y dicen: “tu haces quedar mal al hombre andino, tu haces quedar mal al hombre en sí”. No es que todos los hombres sean violentos pero existe violencia y no hay que ocultarla.&nbsp;Y si a través de mis pinturas puedo denunciarlo, reflejar lo que viví, lo que vi y hacer reflexionar a las personas a través de mi pintura pues lo seguiré haciendo, así a muchos no les guste. Mi intención no es hacer quedar mal a los hombres andinos, en absoluto, pero no podemos negar de que existe violencia hacia las mujeres, sea andina, amazónica o de la ciudad. Existe mucha violencia hacia las mujeres.&nbsp;<br><br>Aquí en Lima nos llaman las “feminazi”. Pero no es así. Yo considero que debe haber un respeto mutuo, un valor, una estima. Todos, todas y todes, somos iguales y merecemos tratarnos con cariño y con amor.</p>
-                                <h1 class="preg">¿Cómo ves el racismo en relación a la violencia de género? ¿Existe algún movimiento que hable sobre el racismo en la comunidad de Sarhua? </h1>
-                                <p class="resp">Yo sé que hay un pintor que ha estudiado asistencia social. Él enseña la pintura a través del muralismo en el pueblo. Pero que se hable exactamente de racismo, no he escuchado. Una vez en 2019 estábamos en el pueblo y me decían los niños: “profesora, hagamos el color piel”. Y cuando hacían el color piel hacían un color blanco, casi rosado. Y cuando habían terminado yo les decía: “¿ese color piel es igual a tu color?” y el niño me decía que no. Así yo le comentaba: “échale un poco más de color, pues este es nuestro color piel, esta es nuestra herencia ¿verdad?”. Algunos también se ponían agua oxigenada en la punta de los cabellos para ser rubios. Y yo les decía: “¿Qué necesidad tienes de ser rubio si tu cabello negro es hermoso?”. No sé si en los colegios se hablé actualmente más de ese tema, pero no he escuchado que exista un movimiento que hable específicamente de racismo.</p>
-                                <h1 class="preg">¿Siempre tuviste autonomía para expresarte en el arte Sarhua? ¿Como fue eso en el proceso de desarrollo del trabajo autoral? </h1>
-                                <p class="resp">Esto es algo heredado, mis padres ambos son pintores. Mi trabajo había sido apoyarlos en el proceso de trabajo de la madera, en el proceso de pintura de los personajes, luego de los detalles. Al comienzo recibía la información de mi padre. Yo no vivía en la comunidad misma, pero él me relacionaba con ellos y con los residentes en Lima. Así yo recibía la información y la registraba. Un día, en 2018, dije: “yo no nací en Sarhua pero tengo esta herencia artística y cultural”. Entonces comencé a reflejar mis vivencias, mi entorno, a las mujeres migrantes. Ahí comencé a pensar en lo que faltaba, lo que no se había mostrado aún. Dije: “Voy a visibilizar a la mujer”. Pensé que la vida me estaba dando un mensaje porque mis padres querían que yo fuera hombre. Me tuvieron a los 42 años y yo salí mujer. La historia de las mujeres no ha sido muy visibilizada. Y allí encontré un vacío y pensé que era tiempo de mostrar mi trabajo, mis intereses como mujer, como heredera del arte, de mis padres de Sarhua. Y así empecé a usar ese medio para hacer escuchar mi voz.</p>
-                                <h1 class="preg">¿La maternidad ha influenciado tu obra? ¿De qué forma?</h1>
-                                <p class="resp">Si mucho, me ha influenciado. Pinto esto en mis temas. Los que conocen mi trabajo siempre van a ver a una mujer con un bebé en la mano, en la espalda. Esto me representa. Yo soy divorciada y no recibo mucho apoyo. Tengo que pensar yo misma por mi hija. Así esté cansada o enferma tengo que seguir adelante porque los niños no entienden que no tengas un pan para darle. ¡Hay que darle de comer! Esto me motiva a buscar soluciones, a madurar y a hacerme responsable de mi actos. Por ello sigo avanzando, visibilizo la fortaleza de las mujeres con hijos. Eso es lo que voy plasmando. Tener una bebé me da mucha fortaleza y me ayuda a madurar.</p>
-                                <h1 class="preg">¿Qué te genera placer en el arte que realizas?</h1>
-                                <p class="resp">Qué bonita pregunta. Disfruto lo que hago, es mi pasión, me gusta estar probando diferentes soportes, temáticas. El hecho de decidirme por la pintura me permite viajar, conocer diferentes personas, propuestas, información, culturas. Siempre le agradezco a mi papá por haberme enseñado lo que él sabia, pues así puedo viajar por el mundo, trasmitir mi voz. Yo también le dejo mis conocimientos y los de mi comunidad a mi hija. Ojalá pueda seguir en esta misión, con mucha visión para seguir avanzando, para conocer más países, más comunidades originarias. Para seguir aprendiendo, conectando con la tierra, con la naturaleza, con nuestra esencia como seres humanos. El arte me ha permitido canalizar diferentes tipos de emociones y sanar muchas heridas también.&nbsp;</p>
+                                <h1 class="preg">¿Cuándo llegaste a Brasil y por qué te estableciste en Minas Gerais?</h1>
+                                <p class="resp">Llegué a Brasil hace nueve años. Lo que sería al principio una visita de un mes pasó a ser mi nueva casa. Antes de venir, viví unos años en Bolivia con mi mamá. Soy hija de artesanos y mi mamá siempre trabajó en ferias o vendiendo en la calle y de esa manera era muy difícil mantenernos en Perú. En Bolivia hay una costumbre mucho más fuerte de comprar en ferias, entonces migramos para allá y nos iba mejor.&nbsp;<br><br>Sin dudas, lo que más me motivó a quedarme en Brasil fue la oportunidad que tenía de poder estudiar en una de las mejores universidades de Brasil, pública y gratuita. Estudiar una carrera en mi país siempre fue como un sueño casi imposible debido a las pocas opciones que hay de universidades públicas. A pesar de los constantes ataques que las universidades públicas brasileñas sufren, considero que en Brasil el acceso a la educación funciona más democráticamente que en Perú.&nbsp;</p>
+                                <h1 class="preg">¿Cómo te acercaste a las artes visuales? </h1>
+                                <p class="resp">Considero que por ser hija de artesanos siempre estuve familiarizada con la “artesanía”. Desde muy pequeña siempre ayudé a mi mamá a trabajar con macramé, alambre, entre otros materiales. Cuando me preparé para postular a la universidad uno de los cursos que más me llamó la atención fue el de Artes Visuales. Hasta entonces recuerdo que las últimas veces que dibujé fue cuando era niña o adolecente, pero siempre estuve por dentro del arte gracias a las danzas populares. Entonces estudiar Artes Visuales para mi fue como encontrar una manera de traer un poco de las danzas populares y todo lo que guardo de mi tierra en mi memoria, para mi producción artística.</p>
+                                <h1 class="preg">¿Tuviste choques culturales entre Brasil y Perú?</h1>
+                                <p class="resp">Si, de hecho lo que más me afectó fue el sentir Brasil muy distante de los países vecinos desde el punto de vista cultural y cómo eso influía en la manera como yo era vista y tratada, siendo una mujer racializada y peruana. Creo que en Brasil se piensa al peruano como algo “exótico” o vinculado a la “pobreza”. Encontrarte entre esa dicotomía por el hecho de ser peruana genera muchos conflictos.</p>
+                                <h1 class="preg">¿Crees que tu trabajo aborda el racismo y la xenofobia? ¿Si es así, de qué manera?&nbsp;</h1>
+                                <p class="resp">Sin dudas. Como dije en la respuesta anterior, considero que por el hecho de ser una inmigrante racializada y venir de un país como Perú, me deparo constantemente con situaciones deplorables. Pienso que las(os)(es) inmigrantes en Brasil principalmente cuando no son blancas(os)(e) y no provienen de países vistos como ricos, son vistas(os)(es) como una amenaza para el país o vinculados a la pobreza. Tengo muchas amistades de inmigrantes que viven en Brasil y constantemente conversamos sobre ello. Por eso he encontrado que mi producción artística puede ser una manera de denunciar y cuestionar sobre esas violencias que nos atraviesan.&nbsp;</p>
+                                <h1 class="preg">Tu obra interactúa mucho con la cultura popular. ¿Cómo ves la relación entre el arte contemporáneo y la cultura popular?</h1>
+                                <p class="resp">Pienso que la cultura popular desde siempre se ha caracterizado por ser impactante, genuina, sofisticada, capaz de llevarnos a muchos lugares. El problema es que ese mundo del arte blanco nunca lo consideró como “arte”. Sin embargo eso está cambiando gracias a mucha lucha y resistencia de las personas que hacen parte de la cultura popular. Pienso que la cultura popular carga múltiples legados, mucha historia y es necesario re direccionar la mirada para ello. </p>
+                                <h1 class="preg">¿Cómo te construyes más allá de la maternidad?</h1>
+                                <p class="resp">Creo que lo hago intentando no perderme. Hay días que me gusta ser mamá y hay otros que no, y eso no habla respecto al amor que tengo por mi hijo. Se refiere a mi propia individualidad. Por eso siempre intento cumplir mis sueños, tener mis ambiciones que no son necesariamente de dinero, son ambiciones de sueños, proyectos, etc. Sé que cumpliéndolos, automáticamente mi hijo también se sentirá muy feliz con ello, ¡al final estamos juntos en el mismo viaje!&nbsp;<br><br>Creo que nos adoctrinan a ponernos en tercero o cuarto lugar cuando somos madres, nos enseñan a que el cuidado y la sobrecarga son sinónimos de “amor y entrega” y que está vinculado al “ser mujer”. Pero eso para mi es una trampa, una estrategia para mantenernos en control. Para que yo sea una mujer y una madre feliz para maternar necesito alimentarme, necesito sentirme realizada.&nbsp;</p>
                             </div>
                         </div>
                     </div>
@@ -80,7 +82,7 @@
             </div>
         </div>
     </div>
-
+    
     <?php include 'pie.php' ?>
 </body>
 </html>
